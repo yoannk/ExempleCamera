eab84544bdc85c7e6499abde3590a038b49ed99e
Yoann Kergall <yoann.kergall@gmail.com>
2020-04-14 11:38:01 +0200

    ajout permissions

--- app/src/main/java/com/example/exemplecamera/MainActivity.java
+++ app/src/main/java/com/example/exemplecamera/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.exemplecamera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
     }
 }
